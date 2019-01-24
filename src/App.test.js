65ffb22dd7d26040c447fb7fcee11e26a4c8828e
shingo asao <asao@core-solution.com>
2019-01-24 17:46:08 +0900

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,62 @@
+import React from "react"
+import Enzyme from "enzyme"
+import { shallow, mount, render } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import store, * as actions from "./modules/counter"
+import App from "./App.js"
 
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("Counter", () => {
+  it("should return the initial state", () => {
+    expect(store(undefined, {})).toEqual({ count: 0, state: 0 })
+  })
+
+  it('this.state.count', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({
+      count: 2,
+    })
+    expect(wrapper.state().count).toBe(2)
+  })
+
+  it('should create an action to increment', () => {
+    const type = 'COUNTER_INCREMENT'
+    const expectedAction = {
+      type
+    }
+    expect(actions.increment(type)).toEqual(expectedAction)
+  })
+
+  it('should create an action to decrement', () => {
+    const type = 'COUNTER_DECREMENT'
+    const expectedAction = {
+      type
+    }
+    expect(actions.decrement(type)).toEqual(expectedAction)
+  })
+
+  it('should handle INCREMENT', () => {
+    expect(
+      store({count: 1}, {
+        type: 'COUNTER_INCREMENT'
+      })
+    ).toEqual(
+      {
+        count: 2
+      }
+    )
+   })
+
+  it('should handle DECREMENT', () => {
+    expect(
+      store({count: 3}, {
+        type: 'COUNTER_DECREMENT'
+      })
+    ).toEqual(
+      {
+        count: 2
+      }
+    )
+   })
+})
