b313e1353b1e44f61e69b7257518f482e834b0be
shingo asao <asao@core-solution.com>
2019-01-29 09:27:23 +0900

    change use redux-actions

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,22 +1,40 @@
-// action type
-const INCREMENT = "COUNTER_INCREMENT"
-const DECREMENT = "COUNTER_DECREMENT"
-
-// action creator
-export function increment() {
-  return { type: INCREMENT }
-}
-
-export function decrement() {
-  return { type: DECREMENT }
-}
+import { createActions, handleActions } from "redux-actions"
 
 // state
 const initialState = {
   count: 0
 }
 
+// action type
+const INCREMENT = "COUNTER_INCREMENT"
+const DECREMENT = "COUNTER_DECREMENT"
+
+// action creator
+export const { increment, decrement } = createActions({
+  INCREMENT: () => ({}),
+  DECREMENT: () => ({}),
+})
+
 // reducer
+export default handleActions({
+  [increment]: state => ({
+    ...state,
+    count: state.count + 1
+  }),
+  [decrement]: state => ({
+    ...state,
+    count: state.count - 1    
+  })
+}, initialState)
+
+/*
+export function increment() {
+  return { type: INCREMENT }
+}
+export function decrement() {
+  return { type: DECREMENT }
+}
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case INCREMENT:
@@ -33,3 +51,4 @@ export default function reducer(state = initialState, action) {
       return state
   }
 }
+*/
